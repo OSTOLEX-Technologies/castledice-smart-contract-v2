a0e979fcedef9159808d7bd2fbf1e406c4abca40
1KitCat1 <denischool119@gmail.com>
2024-01-13 02:15:49 +0200

    Add Game Controller

---
+++ BoardController.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract BoardController {
+    enum BoardState {
+        FREE,
+        BLUE,
+        RED,
+        TREE
+    }
+}
